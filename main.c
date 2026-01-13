c33d4e0f445a2414ef5568ceda888913f3ad5043
Sandro Canalicchio <sanvince@protonmail.com>
2026-01-13 15:48:55 +0100

    initial commit

---
+++ main.c
@@ -0,0 +1,12 @@
+/**
+ * @file main.c
+ * @author sandro
+ * @date 2026-01-13
+ * @brief Main function
+ */
+int main(){
+
+    // Add your code here and press Ctrl + Shift + B to build
+
+    return 0;
+}
